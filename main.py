7a2d7de02e5140c68b197b67ea1de7a663b5dd6b
Михаил Шереметьев <mikhael300010000@gmail.com>
2023-02-04 17:15:15 +0300

    done classes update, saved old main

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 from random import randint
-from graphic_arts.start_game_banner import run_screensaver
 
 
-def attack(char_name: str, char_class: str) -> str:
-    """Battle damage processing."""
-    if char_class == 'warrior':
-        return (f'{char_name} нанёс урон противнику '
-                f'равный {5 + randint(3, 5)}')
-    if char_class == 'mage':
-        return (f'{char_name} нанёс урон противнику '
-                f'равный {5 + randint(5, 10)}')
-    if char_class == 'healer':
-        return (f'{char_name} нанёс урон противнику '
-                f'равный {5 + randint(-3, -1)}')
 
 
 def defence(char_name: str, char_class: str) -> str:
@@ -64,27 +52,6 @@ def start_training(char_name: str, char_class: str) -> str:
     return 'Тренировка окончена.'
 
 
-def choice_char_class() -> str:
-    """Character class selecting."""
-    approve_choice: str = None
-    char_class: str = None
-    while approve_choice != 'y':
-        char_class: str = input('Введи название персонажа, за которого хочешь'
-                                ' играть: Воитель — warrior, Маг — mage,'
-                                ' Лекарь — healer: ')
-        if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя. Сильный, выносливый'
-                  ' и отважный.')
-        if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя. Обладает высоким '
-                  'интеллектом.')
-        if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель. Черпает силы из '
-                  'природы, веры и духов.')
-        approve_choice = input('Нажми (Y), чтобы подтвердить выбор, или любую '
-                               'другую кнопку, чтобы выбрать другого '
-                               'персонажа ').lower()
-    return char_class
 
 
 if __name__ == '__main__':
